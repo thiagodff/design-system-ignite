3f54ad77d1583354696cb75e2f9519f014f1d97b
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-03-19 00:32:25 -0300

    feat(react): config stitches js

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,38 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    // por default o space pega apenas no padding e margin
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
