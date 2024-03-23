f4178cbecea4acd87a731b94001824aad66acbcd
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-03-23 11:02:58 -0300

    feat(docs): button story

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+// config global do button
+const meta = {
+  title: 'Button',
+  component: Button,
+
+  args: {
+    children: 'Enviar',
+  },
+} satisfies Meta<ButtonProps>
+
+export default meta
+type Story = StoryObj<ButtonProps>
+
+export const Small: Story = {}
+
+export const Big: Story = {
+  args: {
+    size: 'big',
+  },
+}
