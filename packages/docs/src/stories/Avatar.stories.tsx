7e0a7433d9b30bcbef627efffa666e0036916a9c
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-03-27 00:57:20 -0300

    feat: add Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+// config global do button
+const meta = {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/thiagodff.png',
+    alt: 'Thiago Dornelles',
+  },
+} satisfies Meta<AvatarProps>
+
+export default meta
+type Story = StoryObj<AvatarProps>
+
+export const Primary: Story = {}
+
+export const WithFallback: Story = {
+  args: {
+    src: undefined,
+  },
+}
