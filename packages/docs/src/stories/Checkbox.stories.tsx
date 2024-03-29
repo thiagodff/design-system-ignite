e54f2ee0409a41a8a15fcaa8d77671c53b3907b2
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-03-29 00:17:27 -0300

    feat: add Checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react'
+
+const meta = {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          <Story />
+          <Text size="sm">Accept terms</Text>
+        </Box>
+      )
+    },
+  ],
+} satisfies Meta<CheckboxProps>
+
+export default meta
+type Story = StoryObj<CheckboxProps>
+
+export const Primary: Story = {}
