80ab053dbfe0a5a18d1ce92765eb8e9dc9536972
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-03-31 12:57:08 -0300

    ci: Add docs workflow

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -18,6 +18,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("storybook-dark-mode"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
